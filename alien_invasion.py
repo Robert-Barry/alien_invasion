13ff8d9352ba1589f92033367eaa920807522053
Robert Barry <rb4080@gmail.com>
2025-08-12 13:34:54 -0400

    Add ship class. Draw ship to the screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,6 +10,7 @@ Because this project is from a book I cannot offer a license for the code.
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -22,6 +23,8 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
         
 
     def run_game(self):
@@ -34,6 +37,7 @@ class AlienInvasion:
 
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             
             # Make the most recently drawn screen visible
             pygame.display.flip()
