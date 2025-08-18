36e7059fd6574bb16e8d1303f18e72a97940f38a
Robert Barry <rb4080@gmail.com>
2025-08-18 14:35:29 -0400

    Add GameStats class. Update functionality to lose and end the game

--- ship.py
+++ ship.py
@@ -23,6 +23,11 @@ class Ship:
         self.moving_right = False
         self.moving_left = False
 
+    def center_ship(self):
+        """Center the ship on the screen."""
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
+
     def update(self):
         """Update the ship's position based on movement flags."""
         # Update the ship's x vaklue, not the rect.
@@ -37,3 +42,5 @@ class Ship:
     def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
+
+    
