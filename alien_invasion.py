78ff1819f98827d7b734c2d7e51ce914feb61721
Robert Barry <rb4080@gmail.com>
2025-08-25 16:15:25 -0400

    Add scoring functionality

--- alien_invasion.py
+++ alien_invasion.py
@@ -107,6 +107,7 @@ class AlienInvasion:
 
             # Reset game statistics
             self.stats.reset_stats()
+            self.sb.prep_score()
             self.game_active = True
 
             # Get rid of any remaining bullets and aliens
@@ -142,6 +143,11 @@ class AlienInvasion:
         """Respond to bullet-alien collisions."""
         # Remove any bullets and aliens that have collided.
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+            self.sb.prep_score()
 
         if not self.aliens:
             # Destroy existing bullets and create new fleat.
